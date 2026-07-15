6d68d874bce24eed74b0a4b88b3f11854343b8e3
Artur <artur.kolomytsev@mail.ru>
2026-07-15 18:29:13 +0300

    KS-3-1 Greeting of User

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "Name"
+    val greetingOfDay = "Good Day"
+    val greetingOfEvening = "Good Evening"
+
+    println("$greetingOfDay, $userName!")
+    println("$greetingOfEvening, $userName!")
+
+}
