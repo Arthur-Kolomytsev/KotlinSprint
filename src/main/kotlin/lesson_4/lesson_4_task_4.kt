f25c26171aa8e9024a7ab7757b13f268a570ddd4
Artur <artur.kolomytsev@mail.ru>
2026-07-21 22:27:02 +0300

    KS-4-4 #3 checking of exercises

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -4,17 +4,17 @@ fun main() {
 
     val dayOfTraining = 5
 
-    val isArmTraining = dayOfTraining == 5
-    val isAbsTraining = dayOfTraining == 5
+    val isArmTraining = dayOfTraining % 2 != 0
+    val isAbsTraining = dayOfTraining % 2 != 0
 
-    val isBackTraining = dayOfTraining != 5
-    val isLegsTraining = dayOfTraining != 5
+    val isBackTraining = dayOfTraining %2 == 0
+    val isLegsTraining = dayOfTraining %2 == 0
 
     println(
         "Упражнения для рук: $isArmTraining\n" +
                 "Упражнения для ног: $isLegsTraining\n" +
                 "Упражнения для спины: $isBackTraining\n" +
-                "Упражнения для пресса: $isAbsTraining\n"
+                "Упражнения для пресса: $isAbsTraining"
     )
 
 }
