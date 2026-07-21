4e62458249fa71f3aef8b115ce30478aa835153d
Artur <artur.kolomytsev@mail.ru>
2026-07-21 23:12:04 +0300

    KS-4-4 #7 added gaps to align the output

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -7,13 +7,13 @@ fun main() {
     val isArmTraining = dayOfTraining % 2 != 0
     val isAbsTraining = dayOfTraining % 2 != 0
 
-    val isBackTraining = dayOfTraining %2 == 0
-    val isLegsTraining = dayOfTraining %2 == 0
+    val isBackTraining = dayOfTraining % 2 == 0
+    val isLegsTraining = dayOfTraining % 2 == 0
 
     println(
-        "Упражнения для рук: $isArmTraining\n" +
-                "Упражнения для ног: $isLegsTraining\n" +
-                "Упражнения для спины: $isBackTraining\n" +
+        "Упражнения для рук:    $isArmTraining\n" +
+                "Упражнения для ног:    $isLegsTraining\n" +
+                "Упражнения для спины:  $isBackTraining\n" +
                 "Упражнения для пресса: $isAbsTraining"
     )
 
