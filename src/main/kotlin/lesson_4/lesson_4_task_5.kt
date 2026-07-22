d8207d7177ca1448c1b1cc4b1af6c5f05d6325d5
Artur <artur.kolomytsev@mail.ru>
2026-07-22 18:39:54 +0300

    KS-4-5 #1 checking conditions for ship leaving

---
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_4
+
+fun main() {
+
+    print("The sheep is damage? true or false:")
+    val input1 = readln()
+    val isDamage = input1.toBoolean()
+
+    print("How many crew members:")
+    val input2 = readln()
+    val crewMembers = input2.toInt()
+
+    print("How many boxes of provision:")
+    val input3 = readln()
+    val provision = input3.toInt()
+
+    print("The weather is good? true or false:")
+    val input4 = readln()
+    val isWeather = input4.toBoolean()
+
+    val isSailingCondition1 = !isDamage && (crewMembers in 55..<70) && (provision > 50) && isWeather
+    val isSailingCondition2 = (crewMembers == 70) && (provision >= 50) && isWeather
+
+    val isSailing = isSailingCondition1 || isSailingCondition2
+
+    if (isSailing) {
+        println("The ship can leaving")
+    } else {
+        println("The ship can not leaving")
+    }
+
+}
