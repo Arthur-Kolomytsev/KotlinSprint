89b83dfd3afd240b5c4ef47e2d1d790c49ceeaa4
Artur <artur.kolomytsev@mail.ru>
2026-07-23 18:59:02 +0300

    KS-5-1 #2 checking if user bot.

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -5,6 +5,6 @@ fun main() {
     print("Решите пример: 5 + 4 = ")
     val input = readln().toIntOrNull()
 
-    if (input == 9) print("Добро пожаловать!") else print("Доступ запрещен")
+    if (input == 9) print("Добро пожаловать!") else print("Доступ запрещен.")
 
 }
