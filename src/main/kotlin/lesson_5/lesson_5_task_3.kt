729052164888e4b963c8622eeae4a564980c338c
Artur <artur.kolomytsev@mail.ru>
2026-07-23 20:34:46 +0300

    KS-5-3 #1 Lucky numbers Quiz

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_5
+
+const val LUCKY_NUMBER_ONE = 10
+const val LUCKY_NUMBER_TWO = 15
+
+
+fun main() {
+
+    print("Введите первое число: ")
+    val numberOne = readln().toInt()
+
+    print("Введите второе число: ")
+    val numberTwo = readln().toInt()
+
+    val isTwoNumbersMatched =
+        (numberOne == LUCKY_NUMBER_ONE || numberOne == LUCKY_NUMBER_TWO) && (numberTwo == LUCKY_NUMBER_ONE || numberTwo == LUCKY_NUMBER_TWO)
+
+    val isOneNumbersMatched =
+        (numberOne == LUCKY_NUMBER_ONE || numberOne == LUCKY_NUMBER_TWO) || (numberTwo == LUCKY_NUMBER_ONE || numberTwo == LUCKY_NUMBER_TWO)
+
+
+    if (isTwoNumbersMatched) println("Поздравляем! Вы выиграли главный приз!")
+    else if (isOneNumbersMatched) println("Вы выиграли утешительный приз!")
+    else println("Неудача!")
+
+
+}
