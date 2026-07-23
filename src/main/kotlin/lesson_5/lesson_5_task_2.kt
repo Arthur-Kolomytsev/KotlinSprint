ba7e3f4a3851b6a3d1916b1980a53d37042e49c4
Artur <artur.kolomytsev@mail.ru>
2026-07-23 19:18:37 +0300

    KS-5-1 #2 checking if user bot.

---
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_5
+
+const val AGE_OG_MAJORITY = 18
+
+fun main() {
+
+    print("Enter your year of birth:")
+    val userYearOfBirth = readln().toInt()
+    val userAge = 2026 - userYearOfBirth
+
+    if (userAge >= AGE_OG_MAJORITY) println("Показать экран со скрытым контентом")
+//    else if (userAge == 16 || userAge == 17) println("Show limited content")
+//    else println("Back to main screen")
+
+
+}
