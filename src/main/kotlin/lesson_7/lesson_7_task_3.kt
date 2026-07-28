6cf2a86caf6ffbdbb6b93843bb1bd1ba15311efd
Artur <artur.kolomytsev@mail.ru>
2026-07-28 21:22:25 +0300

    KS-7-2 #2

---
+++ src/main/kotlin/lesson_7/lesson_7_task_3.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_7
+
+fun main() {
+
+    print("Введите любое число: ")
+    val userNumber = readln().toInt()
+
+    for (i in 0..userNumber step 2) {
+        println("$i")
+    }
+
+}
