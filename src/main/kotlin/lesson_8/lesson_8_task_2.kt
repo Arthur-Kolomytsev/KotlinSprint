d0970401cc37288c7f42e67aeb1996aaffa1d872
Artur <artur.kolomytsev@mail.ru>
2026-07-29 21:45:26 +0300

    KS-8-2 #2

--- src/main/kotlin/lesson_8/lesson_8_task_2.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -4,16 +4,14 @@ fun main() {
 
     val arrayOgIngredients = arrayOf("Помидор", "Лук", "Перец", "Яйца")
 
-    print("Какой ингредиент вас интересует: ")
-    val userIngredient = readln()
 
     for (ingredient in arrayOgIngredients) {
+        print("Введите ингредиент: ")
+        val userIngredient = readln()
         if (ingredient == userIngredient) {
             println("Ингредиент $ingredient в рецепте есть")
-            break
         } else {
             println("Такого ингредиента в рецепте нет")
-            break
         }
     }
 
