52e837576f21ffd6fd7c376f55dbd11f67b8b689
Artur <artur.kolomytsev@mail.ru>
2026-07-30 15:21:26 +0300

    KS-8-3 #5

--- src/main/kotlin/lesson_8/lesson_8_task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_3.kt
@@ -2,9 +2,9 @@ package org.example.lesson_8
 
 fun main() {
 
-    val arrayOgIngredients = arrayOf("яйцо", "помидор", "зелень", "соль", "перец")
+    val arrayOgIngredients = arrayOf("Помидор", "Лук", "Перец", "Яйца")
 
-    println("Какой ингредиент вы хотите найти?")
+    println("Какой ингредидиент вы хотите найти?")
     val userIngredient = readln()
 
     val found = arrayOgIngredients.contains(userIngredient)
