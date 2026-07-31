3bfb2925026e3cd06b8aedf95209b8d3f706751f
Artur <artur.kolomytsev@mail.ru>
2026-07-31 11:59:16 +0300

    KS-9-4 users list of ingredients

---
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_9
+
+fun main() {
+
+    print("Введите ингредиенты: ")
+    val userIngr = readln()
+
+    val ingr = userIngr.split(",")
+    val sortedIngr = ingr.sorted()
+
+    println(sortedIngr)
+
+}
