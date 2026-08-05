1757c9f831bcf3c4dbc123a00544ae0b85099a01
Artur <artur.kolomytsev@mail.ru>
2026-08-05 22:45:03 +0300

    KS-11-3 corrected creation of users and room

--- src/main/kotlin/lesson_11_task_3/Main.kt
+++ src/main/kotlin/lesson_11_task_3/Main.kt
@@ -2,12 +2,10 @@ package org.example.lesson_11_task_3
 
 fun main() {
 
-    val user1 = User(name = "Anna")
-    val user2 = User(name = "Alex")
-    val user3 = User(name = "Ivan")
+    val user1 = User(name = "Anna", avatar = "A")
+    val user2 = User(name = "Alex", avatar = "A")
+    val user3 = User(name = "Ivan", avatar = "I")
 
-    val room1 = Room(cover = "Chat", title = "1")
-
-
+    val room1 = Room(cover = "Chat", title = "1", mutableListOf())
 
 }
