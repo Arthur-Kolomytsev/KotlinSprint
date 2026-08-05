624e2d74b08ae917ad1582432d236663526fe366
Artur <artur.kolomytsev@mail.ru>
2026-08-05 21:14:48 +0300

    KS-11-1 add email to User

--- src/main/kotlin/lesson_11/Main.kt
+++ src/main/kotlin/lesson_11/Main.kt
@@ -2,17 +2,19 @@ package org.example.lesson_11
 
 fun main() {
 
-    val user1 = User(id = 111, login = "user1", password = "12345")
-    val user2 = User(id = 222, login = "user2", password = "54321")
+    val user1 = User(id = 111, login = "user1", password = "12345", email = "user1@gmail.com")
+    val user2 = User(id = 222, login = "user2", password = "54321", email = "user2@gmail.com")
 
     println("user1 id: ${user1.id}\n" +
             "user1 login: ${user1.login}\n" +
-            "user1 password: ${user1.password}")
+            "user1 password: ${user1.password}\n" +
+            "user1 password: ${user1.email}")
 
     println()
 
     println("user2 id: ${user2.id}\n" +
             "user2 login: ${user2.login}\n" +
-            "user2 password: ${user2.password}")
+            "user2 password: ${user2.password}\n" +
+            "user2 password: ${user2.email}")
 
 }
