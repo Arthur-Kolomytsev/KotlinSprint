6b660c24b991c047615aa87f27612f96562240fc
Artur <artur.kolomytsev@mail.ru>
2026-08-10 12:56:18 +0300

    KS-13-2 class PhoneBook2 company by default = null

--- src/main/kotlin/lesson_13_task_2/PhoneBook2.kt
+++ src/main/kotlin/lesson_13_task_2/PhoneBook2.kt
@@ -3,14 +3,14 @@ package org.example.lesson_13_task_2
 class PhoneBook2(
     val name: String,
     val phoneNumber: Long,
-    val company: String?,
+    val company: String? = null,
 
  ) {
 
     fun printContact() {
         val name = name
         val phoneNumber = phoneNumber
-        val company = company ?: "не указано"
+        val company = company ?: "<не указано>"
 
         println("Имя: $name\n" +
                 "Номер: $phoneNumber\n" +
