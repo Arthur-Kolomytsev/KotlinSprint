5ee3b40ebbb8558d058cbdafc2a56632381f4a27
Artur <artur.kolomytsev@mail.ru>
2026-08-10 14:26:08 +0300

    KS-13-3 listOf<PhoneBook3>

--- src/main/kotlin/lesson_13_task_3/Main.kt
+++ src/main/kotlin/lesson_13_task_3/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     val contacts = listOf<PhoneBook3>(
         PhoneBook3(name = "Anna", phoneNumber = 11, company = "company-1"),
         PhoneBook3(name = "Ivan", phoneNumber = 12, company = "company-2"),
-        PhoneBook3(name = "Bob", phoneNumber = 13, company = "company-3"),
+        PhoneBook3(name = "Bob", phoneNumber = 13, company = null),
         PhoneBook3(name = "Alice", phoneNumber = 14, company = "null"),
         PhoneBook3(name = "Max", phoneNumber = 15, company = null),
     )
