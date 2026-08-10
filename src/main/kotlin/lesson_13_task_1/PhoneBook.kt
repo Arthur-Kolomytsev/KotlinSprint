6ef617571437cb44a8869f956ee58dfeba5337fe
Artur <artur.kolomytsev@mail.ru>
2026-08-10 12:29:07 +0300

    KS-13-1 change variable companyName to company

--- src/main/kotlin/lesson_13_task_1/PhoneBook.kt
+++ src/main/kotlin/lesson_13_task_1/PhoneBook.kt
@@ -3,10 +3,9 @@ package org.example.lesson_13_task_1
 class PhoneBook(
     val name: String,
     val phoneNumber: Long,
-    val companyName: String?,
+    val company: String?,
 
-) {
+)
 
 
 
-}
