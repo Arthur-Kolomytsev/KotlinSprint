320af2798001ce9e39b44da4e8fae33751863d11
Artur <artur.kolomytsev@mail.ru>
2026-08-17 13:01:51 +0300

    KS-16-1 Encapsulation private val randomNumber

---
+++ src/main/kotlin/lesson16_task2/Circle.kt
@@ -0,0 +1,32 @@
+package org.example.lesson16_task2
+
+class Circle(var radius: Double) {
+
+    private val pi = 3.14
+
+    var length = 0.0
+        private set
+
+    var field = 0.0
+        private set
+
+    fun calculateLength() {
+        length = 2 * pi * radius
+    }
+
+    fun calculateField() {
+        field = pi * radius * radius
+    }
+
+}
+
+fun main() {
+
+    val c = Circle(radius = 1.5)
+    c.calculateLength()
+    c.calculateField()
+
+    println(c.length)
+    println(c.field)
+
+}
