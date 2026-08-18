fd04c4a7dfba644b74cb055fddcec7d97ac2d3ef
Artur <artur.kolomytsev@mail.ru>
2026-08-18 19:48:35 +0300

    KS-17-5 User login and password

---
+++ src/main/kotlin/lesson17_task5/User.kt
@@ -0,0 +1,25 @@
+package org.example.lesson17_task5
+
+class User(login: String, password: String) {
+
+    var login = login
+        set(value) {
+            field = value
+            println("Login successfully changed")
+        }
+
+    var password = password
+        get() = "*".repeat(field.length)
+        set(value) {
+            println("You can't change the password")
+        }
+
+}
+
+fun main() {
+    val user = User(login = "user1234", password = "12345")
+    println(user.login)
+    println(user.password)
+    user.login = "USER"
+    println(user.login)
+}
