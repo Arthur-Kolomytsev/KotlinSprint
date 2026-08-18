1c126862e4657e40d2b6ab3f7b93cab6c0259b5d
Artur <artur.kolomytsev@mail.ru>
2026-08-18 12:49:49 +0300

    KS-17-2 Ship name setter

---
+++ src/main/kotlin/lesson17_task2/Ship.kt
@@ -0,0 +1,29 @@
+package org.example.lesson17_task2
+
+class Ship(_name: String, _averageSpeed: Int, _homePort: String) {
+
+    var name = _name
+        set(value) {
+            if (value != name) println("Warning! You can't change the name of ship.")
+        }
+
+    val averageSpeed = _averageSpeed
+
+    val homePort = _homePort
+
+    fun showInfo() {
+        println("Ship: $name\n" +
+                "Average Speed: $averageSpeed\n" +
+                "Home Port: $homePort\n")
+    }
+
+}
+
+fun main() {
+
+    val ship = Ship("NAME-1", 10, "Portugal")
+    ship.showInfo()
+    ship.name = "NAME-2"
+    ship.showInfo()
+
+}
