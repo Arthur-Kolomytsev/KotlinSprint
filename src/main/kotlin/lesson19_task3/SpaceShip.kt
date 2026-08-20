303bcfd92148e948195017deed6ccb8d74658888
Artur <artur.kolomytsev@mail.ru>
2026-08-20 17:20:28 +0300

    KS-19-3 TODO

--- src/main/kotlin/lesson19_task3/SpaceShip.kt
+++ src/main/kotlin/lesson19_task3/SpaceShip.kt
@@ -2,13 +2,18 @@ package org.example.lesson19_task3
 
 class SpaceShip(status: Status) {
 
-    fun method1() {
+    fun land() {
         // TODO add some logic
     }
 
-    fun method2() {
+    fun takeOff() {
         TODO()
     }
+
+    fun fireAsteroid() {
+        TODO()
+    }
+
 }
 
 enum class Status {LANDING, TAKE_OFF, FIRE_ASTEROID}
