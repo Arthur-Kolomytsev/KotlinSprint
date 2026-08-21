9b24f6bbc27b1d104e27c667486934f03ab4027f
Artur <artur.kolomytsev@mail.ru>
2026-08-21 22:52:06 +0300

    KS-20-4 Lambda list.map

--- src/main/kotlin/lesson20_task4/Main.kt
+++ src/main/kotlin/lesson20_task4/Main.kt
@@ -4,9 +4,9 @@ fun main() {
 
     val list1 = listOf<String>("X0", "X1", "X2", "X3", "X4")
 
-    val clickList = list1.map { { println("Нажат элемент $it") } }
+    val actions = list1.map { { println("Нажат элемент $it") } }
 
-    clickList.forEachIndexed { index, function ->
+    actions.forEachIndexed { index, function ->
         if ((index + 1) % 2 == 0) {
             function()
         }
