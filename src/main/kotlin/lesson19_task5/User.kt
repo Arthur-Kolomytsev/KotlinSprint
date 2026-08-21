94d8c483f62408debb058405562d85f006326a4a
Artur <artur.kolomytsev@mail.ru>
2026-08-21 13:33:39 +0300

    KS-19-5 Users list

---
+++ src/main/kotlin/lesson19_task5/User.kt
@@ -0,0 +1,46 @@
+package org.example.lesson19_task5
+
+class User private constructor(val name: String, val gender: Gender) {
+
+    override fun toString(): String {
+        return "[Name: $name; Gender: $gender]"
+    }
+
+    companion object {
+
+        fun createUser(name: String, gender: String): User {
+            val gender = if (gender == "man") Gender.MAN else if (gender == "woman") Gender.WOMAN else Gender.UNKNOWN
+            return User(name, gender)
+
+        }
+    }
+}
+
+enum class Gender { MAN, WOMAN, UNKNOWN }
+
+fun main() {
+
+    val users = mutableListOf<User>()
+
+    do {
+        print("Please enter user name: ")
+        val userName = readln()
+
+        print("Please enter user gender (man / woman): ")
+        val userGender = readln()
+
+        val u = User.createUser(userName, userGender)
+        users.add(u)
+
+        if (users.size == 2) {
+            println(users.joinToString(", "))
+            break
+        }
+
+    } while (true)
+
+
+
+
+}
+
