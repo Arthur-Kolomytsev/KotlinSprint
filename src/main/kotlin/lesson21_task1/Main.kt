342615c18a0754d0b5574671bff557b1f9e401ee
Artur <artur.kolomytsev@mail.ru>
2026-08-24 19:18:23 +0300

    KS-21-1 Extension function vowelCount()

--- src/main/kotlin/lesson21_task1/Main.kt
+++ src/main/kotlin/lesson21_task1/Main.kt
@@ -2,7 +2,7 @@ package org.example.lesson21_task1
 
 fun String.vowelCount(): Int {
     val vowels = listOf('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U')
-    return count {it in vowels}
+    return count { it in vowels }
 }
 
 fun main() {
