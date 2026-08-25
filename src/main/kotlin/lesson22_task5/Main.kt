f75b28c999ef407191027155c869c990443abd62
Artur <artur.kolomytsev@mail.ru>
2026-08-25 22:50:00 +0300

    KS-22-5 data class

---
+++ src/main/kotlin/lesson22_task5/Main.kt
@@ -0,0 +1,28 @@
+package org.example.lesson22_task5
+
+import java.time.LocalDateTime
+
+fun main() {
+
+    val alphaCentauri = GalacticGuide(
+        name = "Alpha Centauri",
+        description = "It is the closest star system to the Sun",
+        eventDateTime = LocalDateTime.now(),
+        distanceFromEarth = 4.37
+    )
+
+    val (name, description, eventDateTime, distanceFromEarth) = alphaCentauri
+
+    println("Name: $name")
+    println("Description: $description")
+    println("Date and Time: $eventDateTime")
+    println("Distance from Earth: $distanceFromEarth")
+
+}
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val eventDateTime: LocalDateTime,
+    val distanceFromEarth: Double,
+)
