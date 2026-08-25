fdf481f24a7805317e50c4da0978eabfaf390675
Artur <artur.kolomytsev@mail.ru>
2026-08-25 16:15:05 +0300

    KS-21-3 Extension function Player.isHealthy()

--- src/main/kotlin/lesson21_task3/Main.kt
+++ src/main/kotlin/lesson21_task3/Main.kt
@@ -2,16 +2,18 @@ package org.example.lesson21_task3
 
 fun main() {
 
-    val player1 = Player("X", 85)
-    val player2 = Player("X", 100)
+    val player1 = Player("X", 85, 100)
+    val player2 = Player("X", 100, 100)
 
     println(player1.isHealthy())
     println(player2.isHealthy())
 
 }
 
-class Player(val name: String, var currentHealth: Int) {
-    val maxHealth = 100
-}
+class Player(
+    val name: String,
+    val currentHealth: Int,
+    val maxHealth: Int,
+)
 
 fun Player.isHealthy(): Boolean { return this.currentHealth == this.maxHealth }
