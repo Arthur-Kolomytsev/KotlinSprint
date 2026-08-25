8c0890e14d6cb52e6683e06306b3fde95636fc0f
Artur <artur.kolomytsev@mail.ru>
2026-08-25 22:53:40 +0300

    KS-22-5 data class and component() functions

--- src/main/kotlin/lesson22_task5/Main.kt
+++ src/main/kotlin/lesson22_task5/Main.kt
@@ -11,7 +11,11 @@ fun main() {
         distanceFromEarth = 4.37
     )
 
-    val (name, description, eventDateTime, distanceFromEarth) = alphaCentauri
+    val name = alphaCentauri.component1()
+    val description = alphaCentauri.component2()
+    val eventDateTime = alphaCentauri.component3()
+    val distanceFromEarth = alphaCentauri.component4()
+
 
     println("Name: $name")
     println("Description: $description")
